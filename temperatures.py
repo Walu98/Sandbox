3f13e891adcf15c7d5729145bacc3b03d25126c8
Walu <walu.aier@my.jcu.edu.au>
2017-03-31 18:05:23 +0800

    temperatures

--- temperatures.py
+++ temperatures.py
@@ -5,7 +5,7 @@ def fahrenheit_celsius():
 fahrenheit_celsius()
 
 
-def celsius_fahrenheit():
+def celsius_fahrenheit(): #
     Celsius = int(input("Enter a temperature in Celsius: "))
     Fahrenheit = 9.0 / 5.0 * Celsius + 32
     print("Temperature:", Celsius, "Celsius = ", Fahrenheit, " F")
